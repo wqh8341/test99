6e20724a81e9ae78f9aa148d02c1b25fd7e9d1d8
“wqh8341” <283434726@qq.com>
2019-12-04 14:52:36 +0800

    first commit

--- src/test/java/com/git33/NewTest.java
+++ src/test/java/com/git33/NewTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,35 +15,26 @@ public class NewTest {
 	private WebDriver driver;
 
 
-	  @BeforeClass(alwaysRun = true)
+	  @BeforeClass
 	  public void setUp() throws Exception {
-		 System.setProperty("webdriver.gecko.driver", "D:\\wqh\\eclipse\\git22\\src\\test\\resources\\geckodriver66.exe");
+		  System.setProperty("webdriver.chrome.driver", "D:\\wqh\\eclipse\\com.git33\\src\\test\\resources\\chromedriver782.exe");
+			
+			 driver = new ChromeDriver();
 		
-	    driver = new FirefoxDriver();
+		// System.setProperty("webdriver.gecko.driver", "D:\\wqh\\eclipse\\git22\\src\\test\\resources\\geckodriver66.exe");
+		
+	 //   driver = new FirefoxDriver();
 	
 	    driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 	  }
 
 	  @Test
 	  public void testUntitledTestCase() throws Exception {
-	    driver.get("https://www.baidu.com/");
-	    Thread.sleep(2000);
-	    List<WebElement> elements= driver.findElements(By.tagName("input"));
-	    System.out.println(elements);
-	    WebElement elem= elements.get(7);
-	    elem.sendKeys("testng");
-	    driver.findElement(By.id("su")).click();
-	    
-	    
-	  //  driver.findElement(By.className("s_ipt")).clear();
-	  //  driver.findElement(By.className("s_ipt")).sendKeys("1");
-	   // driver.findElement(By.id("su")).click();
-	//    WebElement elemet= driver.findElement(By.cssSelector("input[type='submit'][class='bg s_btn']"));
-	   // elemet.click();
-	    
-	  //  driver.findElement(By.cssSelector("input[type='submit'][class='bg s_btn']")).click();
-	  //  Thread.sleep(2000);
-	  //  driver.quit();
+			driver.get("http://ask.testfan.cn/");
+			driver.manage().window().maximize();
+		  
+		    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	        driver.quit();
 	    
 	  }
 
